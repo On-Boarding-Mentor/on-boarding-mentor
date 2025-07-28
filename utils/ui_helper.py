486f5d8e16c17b24e720cfd1e9593d6a4dc01dd2
PoYaSharonLin <sharon.lin@iss.nthu.edu.tw>
2025-07-28 16:34:55 +0800

    Update ui_helper.py

--- utils/ui_helper.py
+++ utils/ui_helper.py
@@ -19,17 +19,17 @@ class UIHelper:
     @staticmethod
     def setup_sidebar():
         with st.sidebar:
-            # Group under a header for the main app
-            st.page_link("pages/rag_agents.py",
-                             label="Chat with Notes", icon="📄")
+            st.header("On-boarding Mentor")
+            st.page_link("streamlit_app.py", label="Home", icon="🏠")
 
             # Group related pages under an expander
             with st.expander("Notes & Actions", expanded=True):
+                st.page_link("pages/rag_agents.py",
+                             label="Chat with Notes", icon="📄")
                 st.page_link("pages/documents_upload.py",
                              label="Upload Notes", icon="📁")
                 # st.page_link("pages/action_items.py",
                 #              label="Check Action Items", icon="✅")
-
             selected_lang = st.selectbox(
                 "Language", ["English"],
                 index=0, on_change=UIHelper.save_lang, key="language_select"
